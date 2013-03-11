0fb8156e42bf11be8f44d91b49989f31c30823e1
Eleni Petraki <petraki@cwi.nl>
2013-03-11 12:56:22 +0100

    Deactivate imprints.

--- gdk/gdk_select.c
+++ gdk/gdk_select.c
@@ -1340,7 +1340,7 @@ BATsubselect(BAT *b, BAT *s, const void *tl, const void *th,
 			*  ii) it is not an equi-select, and
 			* iii) is not var-sized.
 			*/
-			use_imprints = 1;
+			use_imprints = 0;
 		}
 		bn = BAT_scanselect(b, s, bn, tl, th, li, hi, equi, anti,
 				    lval, hval, maximum, use_imprints);
