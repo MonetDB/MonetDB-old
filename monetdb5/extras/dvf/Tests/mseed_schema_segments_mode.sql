7cdaa39cc8b08b268965c252183a79645bd712e8
Yagiz Kargin <y.kargin@cwi.nl>
2013-11-15 18:00:48 +0100

    use file_id instead of file_location as file identifier.

--- monetdb5/extras/dvf/Tests/mseed_schema_segments_mode.sql
+++ monetdb5/extras/dvf/Tests/mseed_schema_segments_mode.sql
@@ -4,7 +4,7 @@ CREATE SCHEMA mseed;
 SET SCHEMA mseed;
 
 CREATE TABLE "mseed"."files" (
---	"file_id"  INTEGER,
+	"file_id"  INTEGER,
 	"file_location" STRING,
 -- 	"file_last_modified"	TIMESTAMP,
 	"dataquality"	CHAR(1),
@@ -14,14 +14,14 @@ CREATE TABLE "mseed"."files" (
 	"channel"	VARCHAR(10),
 	"encoding"	TINYINT,
 	"byte_order"	BOOLEAN,
---	CONSTRAINT "files_pkey_file_id" PRIMARY KEY (file_id)
-	CONSTRAINT "files_pkey_file_loc" PRIMARY KEY (file_location)
+	CONSTRAINT "files_pkey_file_id" PRIMARY KEY (file_id)
+-- 	CONSTRAINT "files_pkey_file_loc" PRIMARY KEY (file_location)
 );
 
 
 CREATE TABLE "mseed"."segments" (
---	"file_id" INTEGER,	
-	"file_location" STRING,
+	"file_id" INTEGER,	
+-- 	"file_location" STRING,
 	"segment_id"	INTEGER,
 	"start_time"	TIMESTAMP,
 	"end_time"	TIMESTAMP,
@@ -29,14 +29,16 @@ CREATE TABLE "mseed"."segments" (
 	"frequency"	DOUBLE,
 	"sample_count"   BIGINT,
 	"sample_type"   CHAR(1),
-	CONSTRAINT "segments_file_loc_segment_id_pkey" PRIMARY KEY (file_location, segment_id),
-	CONSTRAINT "segments_fkey_files_file_loc" FOREIGN KEY (file_location) REFERENCES mseed.files(file_location)
+	CONSTRAINT "segments_file_id_segment_id_pkey" PRIMARY KEY (file_id, segment_id),
+	CONSTRAINT "segments_fkey_files_file_id" FOREIGN KEY (file_id) REFERENCES mseed.files(file_id)
+-- 	CONSTRAINT "segments_file_loc_segment_id_pkey" PRIMARY KEY (file_location, segment_id),
+-- 	CONSTRAINT "segments_fkey_files_file_loc" FOREIGN KEY (file_location) REFERENCES mseed.files(file_location)
 );
 
 
 CREATE TABLE "mseed"."data" (
---	"file_id"      INTEGER,
-	"file_location" STRING,
+	"file_id"      INTEGER,
+-- 	"file_location" STRING,
 	"segment_id"       INTEGER,
 	"sample_time"  TIMESTAMP,
 	"sample_value" INTEGER
@@ -47,18 +49,18 @@ CREATE TABLE "mseed"."data" (
 );
 
 CREATE VIEW mseed.metadataview AS
-SELECT f.file_location, dataquality, network, station, location, channel, encoding, byte_order, segment_id, start_time, end_time, prev_gap, frequency, sample_count, sample_type
+SELECT f.file_id, file_location, dataquality, network, station, location, channel, encoding, byte_order, segment_id, start_time, end_time, prev_gap, frequency, sample_count, sample_type
 FROM mseed.files AS f
 	JOIN mseed.segments AS s
-		ON f.file_location = s.file_location;
+		ON f.file_id = s.file_id;
 
 CREATE VIEW mseed.dataview AS
-SELECT f.file_location, dataquality, network, station, location, channel, encoding, byte_order, s.segment_id, start_time, end_time, prev_gap, frequency, sample_count, sample_type, sample_time, sample_value
+SELECT f.file_id, file_location, dataquality, network, station, location, channel, encoding, byte_order, s.segment_id, start_time, end_time, prev_gap, frequency, sample_count, sample_type, sample_time, sample_value
 FROM mseed.files AS f 
 	JOIN mseed.segments AS s
-		ON f.file_location = s.file_location
+		ON f.file_id = s.file_id
 	JOIN mseed.data AS d 
-		ON s.file_location = d.file_location AND s.segment_id = d.segment_id;
+		ON s.file_id = d.file_id AND s.segment_id = d.segment_id;
 
 -- To dump information of the cached files
 CREATE FUNCTION dumpcache()
