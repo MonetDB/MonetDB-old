ccc4f8afdc95097400f55c1e61fe6fb48654a9d0
Minh-Duc Pham <duc@cwi.nl>
2014-03-18 18:02:13 +0100

    Add ontology files
    - Remove ontology file for dbpedia 3.51 and 3.8, only use dbpedia 3.9
    - Add ontology files for eurostat, lubm, swrc, foaf

--- monetdb5/extras/rdf/ontmetadata/loadOntologyToMonetDB.sh
+++ monetdb5/extras/rdf/ontmetadata/loadOntologyToMonetDB.sh
@@ -1,17 +1,14 @@
-NUMMETADATA=`cat ontMetadata.dbpedia.csv | wc -l`
-NUMATTRIBUTES=`cat ontAttribute.dbpedia.csv | wc -l`
+#dbpedia3.9
+NUMMETADATA=`cat ontMetadata.dbpedia39.csv | wc -l`
+NUMATTRIBUTES=`cat ontAttribute.dbpedia39.csv | wc -l`
 
 cp loadOntologySAMPLE.sql loadtmp.sql
 sed -i "s:NUMMETADATA:$NUMMETADATA:g" loadtmp.sql
 sed -i "s:NUMATTRIBUTES:$NUMATTRIBUTES:g" loadtmp.sql
-sed -i "s:MetaFile:${PWD}/ontMetadata.dbpedia.csv:g" loadtmp.sql
-sed -i "s:AttFile:${PWD}/ontAttribute.dbpedia.csv:g" loadtmp.sql
-
-
-
+sed -i "s:MetaFile:${PWD}/ontMetadata.dbpedia39.csv:g" loadtmp.sql
+sed -i "s:AttFile:${PWD}/ontAttribute.dbpedia39.csv:g" loadtmp.sql
 
 mclient < loadtmp.sql
-
 
 
 NUMMETADATA=`cat ontMetadata.gr.csv | wc -l`
@@ -26,7 +23,61 @@ sed -i "s:AttFile:${PWD}/ontAttribute.gr.csv:g" loadtmp.sql
 
 mclient < loadtmp.sql
 
+#lubm
+NUMMETADATA=`cat ontMetadata.lubm.csv | wc -l`
+NUMATTRIBUTES=`cat ontAttribute.lubm.csv | wc -l`
 
+cp loadOntologySAMPLE.sql loadtmp.sql
+sed -i "s:NUMMETADATA:$NUMMETADATA:g" loadtmp.sql
+sed -i "s:NUMATTRIBUTES:$NUMATTRIBUTES:g" loadtmp.sql
+sed -i "s:MetaFile:${PWD}/ontMetadata.lubm.csv:g" loadtmp.sql
+sed -i "s:AttFile:${PWD}/ontAttribute.lubm.csv:g" loadtmp.sql
+
+
+mclient < loadtmp.sql
+
+
+#eurostat
+NUMMETADATA=`cat ontMetadata.eurostat.csv | wc -l`
+NUMATTRIBUTES=`cat ontAttribute.eurostat.csv | wc -l`
+
+cp loadOntologySAMPLE.sql loadtmp.sql
+sed -i "s:NUMMETADATA:$NUMMETADATA:g" loadtmp.sql
+sed -i "s:NUMATTRIBUTES:$NUMATTRIBUTES:g" loadtmp.sql
+sed -i "s:MetaFile:${PWD}/ontMetadata.eurostat.csv:g" loadtmp.sql
+sed -i "s:AttFile:${PWD}/ontAttribute.eurostat.csv:g" loadtmp.sql
+
+
+mclient < loadtmp.sql
+
+
+#swrc
+NUMMETADATA=`cat ontMetadata.swrc.csv | wc -l`
+NUMATTRIBUTES=`cat ontAttribute.swrc.csv | wc -l`
+
+cp loadOntologySAMPLE.sql loadtmp.sql
+sed -i "s:NUMMETADATA:$NUMMETADATA:g" loadtmp.sql
+sed -i "s:NUMATTRIBUTES:$NUMATTRIBUTES:g" loadtmp.sql
+sed -i "s:MetaFile:${PWD}/ontMetadata.swrc.csv:g" loadtmp.sql
+sed -i "s:AttFile:${PWD}/ontAttribute.swrc.csv:g" loadtmp.sql
+
+
+mclient < loadtmp.sql
+
+#foaf
+NUMMETADATA=`cat ontMetadata.foaf.csv | wc -l`
+NUMATTRIBUTES=`cat ontAttribute.foaf.csv | wc -l`
+
+cp loadOntologySAMPLE.sql loadtmp.sql
+sed -i "s:NUMMETADATA:$NUMMETADATA:g" loadtmp.sql
+sed -i "s:NUMATTRIBUTES:$NUMATTRIBUTES:g" loadtmp.sql
+sed -i "s:MetaFile:${PWD}/ontMetadata.foaf.csv:g" loadtmp.sql
+sed -i "s:AttFile:${PWD}/ontAttribute.foaf.csv:g" loadtmp.sql
+
+
+mclient < loadtmp.sql
+
+#List of possible ontologies
 NUMONT=`cat ontList.csv | wc -l`
 
 cp loadOntologyListSAMPLE.sql loadtmp.sql
