0a802b60e58eb3e4554489ec3ea28d84cb0b99e6
Dimitar Nedev <dimitar.nedev@monetdbsolutions.com>
2015-05-28 10:55:00 +0200

    Add SQL procedures for the HTM pre/persist-commit

---
+++ sql/scripts/85_htm.sql
@@ -0,0 +1,11 @@
+-- This Source Code Form is subject to the terms of the Mozilla Public
+-- License, v. 2.0.  If a copy of the MPL was not distributed with this
+-- file, You can obtain one at http://mozilla.org/MPL/2.0/.
+--
+-- Copyright 2008-2015 MonetDB B.V.
+
+CREATE PROCEDURE precommit(id bigint) 
+	EXTERNAL name sql.precommit;
+
+CREATE PROCEDURE persistcommit(id bigint) 
+	EXTERNAL name sql.persistcommit;
