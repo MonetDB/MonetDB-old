d45b339934666058a234e021f6c673bc08db8133
Dimitar Nedev <dimitar.nedev@monetdbsolutions.com>
2015-10-12 16:27:57 +0200

    Add a simple unit test

---
+++ java/embedded/src/test/java/org/monetdb/embedded/test/EmbeddedTest.java
@@ -0,0 +1,39 @@
+/*
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0.  If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ *
+ * Copyright 2008-2015 MonetDB B.V.
+ */
+
+package org.monetdb.embedded.test;
+
+import static org.junit.Assert.assertEquals;
+
+import java.io.File;
+import java.io.IOException;
+import java.sql.SQLException;
+
+import org.junit.Test;
+import org.monetdb.embedded.MonetDBEmbedded;
+import org.monetdb.embedded.result.EmbeddedQueryResult;
+
+public class EmbeddedTest {
+
+	@Test
+	public void simpleTest() throws IOException, SQLException {
+		final File directory;
+		directory = File.createTempFile("temp", Long.toString(System.nanoTime()));
+
+		MonetDBEmbedded db = new MonetDBEmbedded(directory);
+		db.startup(true);
+
+		db.query("CREATE TABLE world (id integer, val integer)");
+		db.query("INSERT INTO world VALUES (1, 10), (2, 20), (3, 30)");
+
+		EmbeddedQueryResult result = db.query("SELECT * FROM world");
+		assertEquals(3, result.getColumn(1).columnSize());
+		
+		result.close();
+	}
+}
