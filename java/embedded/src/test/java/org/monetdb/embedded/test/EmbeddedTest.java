35b9822de9f7733594d6871dd2bbc870045c73c0
Dimitar Nedev <dimitar.nedev@monetdbsolutions.com>
2015-10-21 14:55:38 +0200

    Add new unit tests

--- java/embedded/src/test/java/org/monetdb/embedded/test/EmbeddedTest.java
+++ java/embedded/src/test/java/org/monetdb/embedded/test/EmbeddedTest.java
@@ -15,9 +15,9 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.sql.SQLException;
+import java.util.Iterator;
 
 import org.junit.BeforeClass;
-import org.junit.Ignore;
 import org.junit.Test;
 import org.monetdb.embedded.MonetDBEmbedded;
 import org.monetdb.embedded.result.EmbeddedQueryResult;
@@ -25,6 +25,9 @@ import org.monetdb.embedded.result.EmbeddedQueryResult;
 public class EmbeddedTest {
 	static File datbaseDirectory;
 	static MonetDBEmbedded db;
+
+	static Integer[] testValues = {Integer.valueOf(10), Integer.valueOf(20), Integer.valueOf(30), null};
+
 	static Object[] numbericTypeTestValues = new Object[]{
 			Byte.valueOf((byte)12),
 			Short.valueOf((short)23),
@@ -51,10 +54,10 @@ public class EmbeddedTest {
 		db = new MonetDBEmbedded(datbaseDirectory);
 		db.startup(false);
 
-		db.query("CREATE TABLE world (id integer, val integer);");
-		db.query("INSERT INTO world VALUES (1, 10), (2, 20), (3, 30), (4, null);");
+		db.query("CREATE TABLE test (id integer, val integer);");
+		db.query("INSERT INTO test VALUES (0, " + testValues[0] + "), (1, " + testValues[1] + "), (2, " + testValues[2] + "), (3, " + testValues[3] + ");");
 
-		db.query("CREATE TABLE numeric_types_test (fbyte tinyint, fshort smallint, fint integer, flong bigint, ffloat float, fdouble double);");
+		db.query("CREATE TABLE numeric_types_test (fbyte tinyint, fshort smallint, fint integer, flong bigint, freal real, fdouble double);");
 		db.query("INSERT INTO numeric_types_test VALUES (" + numbericTypeTestValues[0] + ", " + numbericTypeTestValues[1] + ", " + numbericTypeTestValues[2] + ", " 
 				+ numbericTypeTestValues[3] + ", " + numbericTypeTestValues[4] + ", " + numbericTypeTestValues[5] + ");");
 		db.query("INSERT INTO numeric_types_test VALUES (null, null, null, null, null, null);");
@@ -74,7 +77,7 @@ public class EmbeddedTest {
 		MonetDBEmbedded restartedDB = new MonetDBEmbedded(datbaseDirectory);
 		restartedDB.startup(false);
 
-		try (EmbeddedQueryResult result = restartedDB.query("SELECT * FROM world;")) {
+		try (EmbeddedQueryResult result = restartedDB.query("SELECT * FROM test;")) {
 			assertEquals(4, result.getColumn(1).columnSize());
 			assertEquals(Integer.valueOf(20), result.getColumn(1).getVaule(1));
 			assertEquals(null, result.getColumn(1).getVaule(3));
@@ -82,11 +85,34 @@ public class EmbeddedTest {
 	}
 
 	@Test
+	public void iteratorTest() throws IOException, SQLException {
+		try (EmbeddedQueryResult result = db.query("SELECT * FROM test;")) {
+
+			int i = 0;
+			Iterator<?> iterator = result.getColumn(1).iterator();
+			while (iterator.hasNext()) {
+				assertEquals(testValues[i++], iterator.next());
+			}
+		}
+	}
+
+	@Test
 	public void integerWithNullTest() throws IOException, SQLException {
-		try (EmbeddedQueryResult result = db.query("SELECT * FROM world;")) {
+		try (EmbeddedQueryResult result = db.query("SELECT * FROM test;")) {
 			assertEquals(4, result.getColumn(1).columnSize());
 			assertEquals(Integer.valueOf(20), result.getColumn(1).getVaule(1));
 			assertEquals(null, result.getColumn(1).getVaule(3));
+		}
+	}
+
+	@Test
+	public void getColumnByNameTest() throws IOException, SQLException {
+		try (EmbeddedQueryResult result = db.query("SELECT * FROM numeric_types_test;")) {
+			assertEquals(6, result.getNumberOfColumns());
+
+			// byte
+			assertEquals(numbericTypeTestValues[2], result.getColumn("fint").getVaule(0));
+			assertEquals(null, result.getColumn("fint").getVaule(1));
 		}
 	}
 
@@ -95,21 +121,22 @@ public class EmbeddedTest {
 		try (EmbeddedQueryResult result = db.query("SELECT * FROM numeric_types_test;")) {
 			assertEquals(6, result.getNumberOfColumns());
 
+			// byte
 			assertEquals(numbericTypeTestValues[0], result.getColumn(0).getVaule(0));
 			assertEquals(null, result.getColumn(0).getVaule(1));
-
+			// short
 			assertEquals(numbericTypeTestValues[1], result.getColumn(1).getVaule(0));
 			assertEquals(null, result.getColumn(1).getVaule(1));
-
+			// int
 			assertEquals(numbericTypeTestValues[2], result.getColumn(2).getVaule(0));
 			assertEquals(null, result.getColumn(2).getVaule(1));
-
+			// long
 			assertEquals(numbericTypeTestValues[3], result.getColumn(3).getVaule(0));
 			assertEquals(null, result.getColumn(3).getVaule(1));
-
+			// float
 			assertEquals(numbericTypeTestValues[4], result.getColumn(4).getVaule(0));
 			assertEquals(null, result.getColumn(4).getVaule(1));
-
+			// double
 			assertEquals(numbericTypeTestValues[5], result.getColumn(5).getVaule(0));
 			assertEquals(null, result.getColumn(5).getVaule(1));
 		}
@@ -139,13 +166,13 @@ public class EmbeddedTest {
 	}
 
 	@Test
-	public void twoQueries() throws IOException, SQLException {
-		EmbeddedQueryResult result1 = db.query("SELECT * FROM world WHERE id > 2;");
+	public void twoQueries() throws SQLException {
+		EmbeddedQueryResult result1 = db.query("SELECT * FROM test WHERE id > 1;");
 		assertEquals(2, result1.getColumn(1).columnSize());
 		assertEquals(Integer.valueOf(30), result1.getColumn(1).getVaule(0));
 		assertEquals(null, result1.getColumn(1).getVaule(1));
 
-		EmbeddedQueryResult result2 = db.query("SELECT * FROM world WHERE id < 2;");
+		EmbeddedQueryResult result2 = db.query("SELECT * FROM test WHERE id < 1;");
 		assertEquals(1, result2.getColumn(1).columnSize());
 		assertEquals(Integer.valueOf(10), result2.getColumn(1).getVaule(0));
 
@@ -157,7 +184,7 @@ public class EmbeddedTest {
 	@Test
 	public void manualCleanupTest() throws IOException, SQLException {
 		@SuppressWarnings("resource")
-		EmbeddedQueryResult result = db.query("SELECT * FROM world;");
+		EmbeddedQueryResult result = db.query("SELECT * FROM test;");
 		assertEquals(4, result.getColumn(1).columnSize());
 		assertEquals(Integer.valueOf(20), result.getColumn(1).getVaule(1));
 		assertEquals(null, result.getColumn(1).getVaule(3));
@@ -167,11 +194,10 @@ public class EmbeddedTest {
 
 	@Test(expected=SQLException.class)
 	public void captureQueryErrorTest() throws SQLException {
-		db.query("SELECT FROM world;");
+		db.query("SELECT FROM test;");
 	}
 
 	@Test
-	@Ignore
 	public void newDatabaseTest() throws IOException, SQLException {
 		final Path tempDirectoryPath = Files.createTempDirectory("monetdbtest_new");
 		final File newDirectory = tempDirectoryPath.toFile();
@@ -179,8 +205,8 @@ public class EmbeddedTest {
 		MonetDBEmbedded newDB = new MonetDBEmbedded(newDirectory);
 		newDB.startup(false);
 
-		newDB.query("CREATE TABLE world (id integer, val integer);");
-		newDB.query("INSERT INTO world VALUES (1, 10), (2, 20), (3, 30), (4, null);");
+		newDB.query("CREATE TABLE test (id integer, val integer);");
+		newDB.query("INSERT INTO test VALUES (1, 10), (2, 20), (3, 30), (4, null);");
 
 		try (EmbeddedQueryResult result = newDB.query("SELECT * FROM world;")) {
 			assertEquals(4, result.getColumn(1).columnSize());
