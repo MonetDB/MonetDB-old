7d7a33e021cabba9648d0958736d445d2582fde3
Martin Kersten <mk@cwi.nl>
2016-06-22 15:02:25 +0200

    Simplify test

--- sql/backends/monet5/iot/Tests/iot12.sql
+++ sql/backends/monet5/iot/Tests/iot12.sql
@@ -3,8 +3,8 @@ set schema iot;
 set optimizer='iot_pipe';
 
 -- we don't have global variables
-create stream table clocks(cnt integer,clk1 integer, clk2 integer);
-insert into clocks values(0,0,0);
+create stream table clocks(cnt integer,clk1 integer);
+insert into clocks values(0,0);
 
 call iot.heartbeat('iot','clocks',1000);
 create procedure clk1()
@@ -14,17 +14,8 @@ begin
 		cnt = cnt +1;
 end;
 
-create procedure clk3()
-begin
-	update clocks
-		set clk1 = clk1+1,
-			clk2 = clk2+2,
-			cnt = cnt +1;
-end;
-
 -- alternative is a simple query
 call iot.query('iot','clk1');
-call iot.query('iot','clk3');
 call iot.pause();
 
 --select * from  iot.baskets();
@@ -38,6 +29,5 @@ select * from clocks;
 call iot.stop();
 select * from iot.errors();
 drop procedure clk1;
-drop procedure clk3;
 drop table clocks;
 
