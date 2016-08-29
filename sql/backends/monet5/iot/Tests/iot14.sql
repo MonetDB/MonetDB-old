aa452dc788603ab29cf822666047367c38291b3d
Martin Kersten <mk@cwi.nl>
2016-08-29 11:20:53 +0200

    Add a new test

--- sql/backends/monet5/iot/Tests/iot14.sql
+++ sql/backends/monet5/iot/Tests/iot14.sql
@@ -4,18 +4,6 @@ set optimizer='iot_pipe';
 
 create stream table sdel (t timestamp, sensor integer, val decimal(8,2)) ;
 
-insert into sdel values('2005-09-23 12:34:26.736',1,12.34);
-insert into sdel values('2005-09-23 12:34:26.736',2,12.34);
-insert into sdel values('2005-09-23 12:34:26.736',3,12.34);
-insert into sdel values('2005-09-23 12:34:26.736',4,12.34);
-select * from sdel;
-
-delete from sdel where sensor = 2;
-select * from sdel;
-
--- don't remove tuples automatically
-call iot.tumble('iot','sdel',0);
-
 create procedure sdel00()
 begin
 	delete from sdel where sensor = 3;
