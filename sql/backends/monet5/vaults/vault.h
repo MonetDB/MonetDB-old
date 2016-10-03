5861f92d0c235d0620e09916638434b217e8fa52
Romulo Goncalves <goncalve@cwi.nl>
2016-10-03 12:42:11 +0200

    Generic checktable, analyzetable, tid and bind. Enum for vault status and reader

--- sql/backends/monet5/vaults/vault.h
+++ sql/backends/monet5/vaults/vault.h
@@ -28,13 +28,33 @@
 
 #define _VAULT_DEBUG_
 
-vault_export str VLTprelude(Client cntxt, MalBlkPtr mb, MalStkPtr stk, InstrPtr pci);
+//vault_export str VLTprelude(Client cntxt, MalBlkPtr mb, MalStkPtr stk, InstrPtr pci);
+vault_export str VLTprelude(void *ret);
 vault_export str VLTimport(Client cntxt, MalBlkPtr mb, MalStkPtr stk, InstrPtr pci);
 vault_export str VLTsetLocation(str *ret, str *src);
 vault_export str VLTgetLocation(str *ret);
 vault_export str VLTremove(timestamp *ret, str *t);
 vault_export str VLTbasename(str *ret, str *fnme, str *splot);
-vault_export  str VLTepilogue(void *ret);
+vault_export str VLTepilogue(void *ret);
+
+vault_export str VLTcheckTable(Client cntxt, MalBlkPtr mb, MalStkPtr stk, InstrPtr pci);
+vault_export str VLTanalyzeTable(Client cntxt, MalBlkPtr mb, MalStkPtr stk, InstrPtr pci);
+vault_export str mvc_VLT_bind_wrap(Client cntxt, MalBlkPtr mb, MalStkPtr stk, InstrPtr pci);
+vault_export str VLTTid(Client cntxt, MalBlkPtr mb, MalStkPtr stk, InstrPtr pci);
+
 
 vault_export char vaultpath[BUFSIZ];
+
+typedef enum {
+    VAULT_TABLE_UNLOADED,
+    VAULT_TABLE_LOADED,
+    VAULT_TABLE_ANALYZE,
+    VAULT_TABLE_DONE
+} VAULT_STATUS;
+
+typedef enum {
+    VAULT_LIDAR_READER = 1,
+    VAULT_GADGET_READER = 2
+} VAULT_READER;
+
 #endif /* _VAULT_H */
