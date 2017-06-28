b6320732073a1fde8ba7839fa840c3cd7e9db9c0
Mark Raasveldt <m.raasveldt@cwi.nl>
2017-06-28 11:33:57 +0200

    Remove old code.

--- sql/backends/monet5/sample/Tests/weightedsample.sql
+++
@@ -1,9 +0,0 @@
-set optimizer = 'sequential_pipe';
--- ADD FLAG TO DISALLOW PARALLELIZATION (MITOSIS) FOR weighted_sample
-CREATE TABLE wsample (i INTEGER, weights DOUBLE);
-INSERT INTO wsample VALUES (1, 1), (2, 1), (3, 1), (4, 1), (5, 1);
-
-
-explain SELECT i FROM wsample WHERE weighted_sample(weights, 2);
-SELECT i FROM wsample WHERE weighted_sample(weights, 2);
-
