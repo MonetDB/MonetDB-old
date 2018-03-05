d62eab1826ac9ee5c83ec9a8d512de06eee4dc42
Aris Koning <aris.koning@monetdbsolutions.com>
2018-03-05 15:56:39 +0100

    Implementation seems to be working on Windows as output of test suggests.

--- gdk/Tests/push_and_check_rss.sql
+++ gdk/Tests/push_and_check_rss.sql
@@ -8,22 +8,21 @@ create table test(a int, b int, c double);
 
 insert into test VALUES (1, 0, 1);
 
-select getrss() as resident_set_size, getvm() as virtual_memory_size;
-
-create procedure loop_insert(n bigint)
+create procedure loop_insert(maximum_size bigint)
 BEGIN
-    declare run BIGINT;
-    set run = (select count(*) from test);
+    declare size BIGINT;
+    set size = (select count(*) from test);
 
-    WHILE run < n DO
+    WHILE size < maximum_size DO
         insert into test (select a+1, b+2, rand()*c from test);
 
-        set run = (select count(*) from test);
+        set size = (select count(*) from test);
     END WHILE;
 END;
 
 call loop_insert(10000000);
 
-select count(*) from test;
+-- It seems that it requires an analytical query to keep memory in RAM.
+select count(*) as record_count from test;
 
 select getrss() as resident_set_size, getvm() as virtual_memory_size;
