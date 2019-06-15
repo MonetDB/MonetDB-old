a8d45745b25a8a91b75351c9c8184babc34164fb
Niels Nes <niels@cwi.nl>
2019-06-15 14:35:32 +0200

    no need for symbol_cmp without the qc

--- sql/server/sql_mvc.h
+++ sql/server/sql_mvc.h
@@ -259,6 +259,4 @@ extern sql_part *mvc_copy_part(mvc *m, sql_table *t, sql_part *pt);
 extern void *sql_error(mvc *sql, int error_code, _In_z_ _Printf_format_string_ char *format, ...)
 	__attribute__((__format__(__printf__, 3, 4)));
 
-extern int symbol_cmp(mvc* sql, symbol *s1, symbol *s2);
-
 #endif /*_SQL_MVC_H*/
