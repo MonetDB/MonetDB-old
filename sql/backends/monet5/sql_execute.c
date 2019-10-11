e3d823f3fddd6b53c6b40bb85f414533d6d335f0
Thodoris Zois <thodoris.zois@monetdbsolutions.com>
2019-10-11 11:28:55 +0200

    minor

--- sql/backends/monet5/sql_execute.c
+++ sql/backends/monet5/sql_execute.c
@@ -16,7 +16,6 @@
  */
 #include "monetdb_config.h"
 #include "mal_backend.h"
-#include "gdk_tracer.h"
 #include "sql_scenario.h"
 #include "sql_result.h"
 #include "sql_gencode.h"
@@ -39,6 +38,7 @@
 #include "mtime.h"
 #include "optimizer.h"
 #include "opt_inline.h"
+#include "gdk_tracer.h"
 #include <unistd.h>
 
 /*
