7aa2854ee64931bf79c9acf53192498c92112afa
Thodoris Zois <thodoris.zois@monetdbsolutions.com>
2019-11-07 17:23:21 +0100

    Merge with default

--- gdk/gdk_system.h
+++ gdk/gdk_system.h
@@ -304,8 +304,7 @@ typedef struct MT_Lock {
 	do {							\
 		assert((l)->lock == NULL);			\
 		(l)->lock = CreateMutex(NULL, 0, NULL);		\
-		strncpy((l)->name, (n), sizeof((l)->name));	\
-		(l)->name[sizeof((l)->name) - 1] = 0;		\
+		strcpy_len((l)->name, (n), sizeof((l)->name));	\
 		_DBG_LOCK_INIT(l);				\
 	} while (0)
 
@@ -376,8 +375,7 @@ typedef struct MT_Lock {
 #define MT_lock_init(l, n)					\
 	do {							\
 		pthread_mutex_init(&(l)->lock, 0);		\
-		strncpy((l)->name, (n), sizeof((l)->name));	\
-		(l)->name[sizeof((l)->name) - 1] = 0;		\
+		strcpy_len((l)->name, (n), sizeof((l)->name));	\
 		_DBG_LOCK_INIT(l);				\
 	} while (0)
 
@@ -507,8 +505,7 @@ typedef struct {
 #define MT_sema_init(s, nr, n)						\
 	do {								\
 		assert((s)->sema == NULL);				\
-		strncpy((s)->name, (n), sizeof((s)->name));		\
-		(s)->name[sizeof((s)->name) - 1] = 0;			\
+		strcpy_len((s)->name, (n), sizeof((s)->name));		\
 		(s)->sema = CreateSemaphore(NULL, nr, 0x7fffffff, NULL); \
 	} while (0)
 
@@ -545,8 +542,7 @@ typedef struct {
 
 #define MT_sema_init(s, nr, n)						\
 	do {								\
-		strncpy((s)->name, (n), sizeof((s)->name));		\
-		(s)->name[sizeof((s)->name) - 1] = 0;			\
+		strcpy_len((s)->name, (n), sizeof((s)->name));		\
 		(s)->sema = dispatch_semaphore_create((long) (nr));	\
 	} while (0)
 
@@ -567,8 +563,7 @@ typedef struct {
 
 #define MT_sema_init(s, nr, n)					\
 	do {							\
-		strncpy((s)->name, (n), sizeof((s)->name));	\
-		(s)->name[sizeof((s)->name) - 1] = 0;		\
+		strcpy_len((s)->name, (n), sizeof((s)->name));	\
 		(s)->cnt = (nr);				\
 		pthread_mutex_init(&(s)->mutex, 0);		\
 		pthread_cond_init(&(s)->cond, 0);		\
@@ -616,8 +611,7 @@ typedef struct {
 
 #define MT_sema_init(s, nr, n)					\
 	do {							\
-		strncpy((s)->name, (n), sizeof((s)->name));	\
-		(s)->name[sizeof((s)->name) - 1] = 0;		\
+		strcpy_len((s)->name, (n), sizeof((s)->name));	\
 		sem_init(&(s)->sema, 0, nr);			\
 	} while (0)
 
