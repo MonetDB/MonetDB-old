9e2dcf5bc86ff1ea1a6c95f8d18bf6d46a740f2d
Thodoris Zois <thodoris.zois@monetdbsolutions.com>
2020-02-21 10:24:17 +0100

    Added module hitchhiker (hh) in SQL layer

---
+++ sql/backends/monet5/sql_hitchhiker.h
@@ -0,0 +1,26 @@
+/*
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0.  If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ *
+ * Copyright 1997 - July 2008 CWI, August 2008 - 2020 MonetDB B.V.
+ */
+
+#ifndef _SQL_HITCHHIKER_
+#define _SQL_HITCHHIKER_
+
+#include "sql.h"
+
+#ifdef WIN32
+#ifndef LIBSQL
+#define sql5_export extern __declspec(dllimport)
+#else
+#define sql5_export extern __declspec(dllexport)
+#endif
+#else
+#define sql5_export extern
+#endif
+
+sql5_export str sql_hitchhiker(Client cntxt, MalBlkPtr mb, MalStkPtr stk, InstrPtr pci);
+
+#endif /* _SQL_HITCHHIKER_ */
